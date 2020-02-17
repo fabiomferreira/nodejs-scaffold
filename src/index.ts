26b2d4937be8fba657fc6503baa29b7ff2481a53
Fabio Ferreira <fabiomf@protonmail.com>
2020-02-17 12:16:40 -0400

    Create index route

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// routes
+app.get('/', (req:express.Request, res: express.Response, next) => {
+  res.send('Welcome to this API');
+});
+
+
+app.listen(port);
