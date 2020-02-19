96009836b8ec8b6f922d2d0bfb317d5265f43a8c
Fabio Ferreira <fabiomf@protonmail.com>
2020-02-19 12:37:29 -0400

    Use database environment variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,10 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/nodejs-scaffold');
+const databaseUrl = process.env.DATABASE_URL || 'localhost:5432';
+const pgUser = process.env.PGUSER || 'postgress';
+const pgPassword = process.env.PGPASSWORD || 'postgress';
+
+const sequelize = new Sequelize(`postgres://${pgUser}:${pgPassword}@${databaseUrl}/nodejs-scaffold`);
 
 sequelize.sync();
 
