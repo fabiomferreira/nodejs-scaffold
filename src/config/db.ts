7fe0a6c268c2ce737ed21c2fe41c359311821e90
Fabio Ferreira <fabiomf@protonmail.com>
2020-02-19 12:47:24 -0400

    pg config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize';
 
 const databaseUrl = process.env.DATABASE_URL || 'localhost:5432';
-const pgUser = process.env.PGUSER || 'postgress';
-const pgPassword = process.env.PGPASSWORD || 'postgress';
+const pgUser = process.env.PGUSER || 'postgres';
+const pgPassword = process.env.PGPASSWORD || 'postgres';
+console.log(process.env)
 
 const sequelize = new Sequelize(`postgres://${pgUser}:${pgPassword}@${databaseUrl}/nodejs-scaffold`);
 
